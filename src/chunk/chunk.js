8ca0cce1a6ce85a5aacdd194b7a0379fc3ee315b
Roman <@>
2023-07-21 19:52:53 +0400

    feat: add comments to chunk func

--- src/chunk/chunk.js
+++ src/chunk/chunk.js
@@ -1,3 +1,12 @@
+// Creates an array of elements split into groups the length of size. 
+// If array can't be split evenly, the final chunk will be the remaining elements.
+
+// _.chunk(['a', 'b', 'c', 'd'], 2);
+// => [['a', 'b'], ['c', 'd']]
+ 
+// _.chunk(['a', 'b', 'c', 'd'], 3);
+// => [['a', 'b', 'c'], ['d']]
+
 function chunkArray(array, size) {
     if (!Array.isArray(array) || size <= 0) {
       throw new Error('Invalid input: "array" should be an array and "size" should be a positive number.');
