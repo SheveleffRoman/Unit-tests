dd8bef573baa3762c8d9375e6612787bc44a4a13
Roman <@>
2023-07-21 21:11:36 +0400

    feat: add slice method

--- src/service/service.js
+++ src/service/service.js
@@ -2,6 +2,20 @@ const service = {
     push: (arr, item) => {
         arr[arr.length] = item;
     },
+    slice: (arr, start, end) => {
+        const  temp =[];
+        if(!Array.isArray(arr)) {
+            return new Error('not an array');
+        }
+        if(end === undefined || end > arr.length) {
+            end = arr.length;
+        }
+        for (let i = start; i < end; i++) {
+            service.push(temp, arr[i]);
+        }
+        return temp;
+
+    }
 }
 
 module.exports = service;
