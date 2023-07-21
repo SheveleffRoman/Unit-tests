13a54b0b51e01964b21b0c8e513d5664c44e6113
Roman <@>
2023-07-21 20:22:24 +0400

    feat: add compact function and test

---
+++ src/compact/compact.js
@@ -0,0 +1,22 @@
+// Creates an array with all falsey values removed. 
+// The values false, null, 0, "", undefined, and NaN are falsey.
+
+// _.compact([0, 1, false, 2, '', 3]);
+// => [1, 2, 3]
+
+const compact = (array) => {
+    if (!Array.isArray(array)) {
+      throw new Error('Invalid input: "array" should be an array.');
+    }
+  
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i]) {
+        result[result.length] = array[i];
+      }
+    }
+  
+    return result;
+  };
+
+  module.exports = compact;
